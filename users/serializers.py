136d60c2ff6fd3fe903941ede53bd0418bfafa80
Hristyaa <shhs200799@gmail.com>
2025-12-23 18:51:55 +0900

    PaymentListAPIView with filters and ordering

--- users/serializers.py
+++ users/serializers.py
@@ -6,4 +6,4 @@ from users.models import Payment
 class PaymentSerializer(ModelSerializer):
     class Meta:
         model = Payment
-        fields = '__all__'
+        fields = "__all__"
--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework.generics import ListAPIView
 
 from users.models import Payment
@@ -5,5 +7,10 @@ from users.serializers import PaymentSerializer
 
 
 class PaymentListAPIView(ListAPIView):
+    """Вывод списка платежей с возможностью фильтрации по курсу, уроку и способу оплаты, сортировке по дате оплаты"""
+
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ("paid_course", "paid_lesson", "payment_method")
+    ordering_fields = ("date_payed",)
