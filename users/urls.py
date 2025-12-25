585e3536fc0b66c3aa38c4e4f8bb1f8a043c5ec4
Hristyaa <shhs200799@gmail.com>
2025-12-25 21:58:59 +0900

    permission_classes=(AllowAny,) for register, login/, token/refresh/

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
 from users.apps import UsersConfig
@@ -11,8 +12,8 @@ app_name = UsersConfig.name
 urlpatterns = [
     path("payments/", PaymentListAPIView.as_view(), name="payments_list"),
     path("register/", UserCreateAPIView.as_view(), name="register"),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token_refresh"),
     path("", UserListAPIView.as_view(), name="lesson_list"),
     path("<int:pk>/", UserRetrieveAPIView.as_view(), name="user_get"),
     path(
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView, \
     RetrieveAPIView
+from rest_framework.permissions import AllowAny
 
 from users.models import Payment, User
 from users.serializers import PaymentSerializer, UserSerializer
@@ -21,6 +22,7 @@ class UserCreateAPIView(CreateAPIView):
     """Регистрация пользователя"""
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = (AllowAny,)
 
     def perform_create(self, serializer):
         user = serializer.save(is_active=True)
