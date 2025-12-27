e53e662ffa87a55bae0e763bd9c2e48cbf1ecde3
Hristyaa <shhs200799@gmail.com>
2025-12-27 19:42:32 +0900

    get_permissions for Course IsOwner (update, retrieve, destroy)

--- materials/views.py
+++ materials/views.py
@@ -6,7 +6,7 @@ from rest_framework.viewsets import ModelViewSet
 from materials.models import Course, Lesson
 from materials.serializers import (CourseDetailSerializer, CourseSerializer,
                                    LessonSerializer)
-from users.permissions import IsModers
+from users.permissions import IsModers, IsOwner
 
 
 class CourseViewSet(ModelViewSet):
@@ -25,11 +25,17 @@ class CourseViewSet(ModelViewSet):
         course.save()
 
     def get_permissions(self):
-        """ Проверка прав пользователя - модератор может только изменять и просматривать курсы"""
-        if self.action in ['create', 'destroy']:
+        """
+        Проверка прав пользователя:
+        Модератор может просматривать и редактировать любые уроки и курсы, но не может удалять и создавать уроки и курсы.
+        Пользователи, которые не входят в группу модераторов, могли видеть, редактировать и удалять только свои курсы и уроки.
+        """
+        if self.action == 'create':
             self.permission_classes = (~IsModers,)
         elif self.action in ['update', 'retrieve']:
-            self.permission_classes = (IsModers,)
+            self.permission_classes = (IsModers | IsOwner, )
+        elif self.action == 'destroy':
+            self.permission_classes = (IsOwner | ~IsModers,)
         return super().get_permissions()
 
 
--- users/permissions.py
+++ users/permissions.py
@@ -2,7 +2,14 @@ from rest_framework.permissions import BasePermission
 
 
 class IsModers(BasePermission):
-
+    """ Проверка принадлежности пользователя к группе 'Модераторы'"""
     def has_permission(self, request, view):
-        """ Проверка принадлежности пользователя к группе 'Модераторы'"""
         return request.user.groups.filter(name='moders').exists()
+
+
+class IsOwner(BasePermission):
+    """ Проверка, является ли пользователь владельцем"""
+    def has_object_permission(self, request, view, obj):
+        if obj.owner == request.user:
+            return True
+        return False
